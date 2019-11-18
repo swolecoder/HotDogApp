f87ef8de0bb05b89fb22b510bb6a0fc56b76c976
Ashish Ranjan <swolecoder@gmail.com>
2019-11-18 10:39:22 -0500

    adding data

--- HotDog/ViewController.swift
+++ HotDog/ViewController.swift
@@ -11,7 +11,7 @@ import CoreML;
 import Vision;
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
     let imagePicker = UIImagePickerController();
@@ -20,11 +20,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         imagePicker.delegate = self;
-         imagePicker.sourceType = .camera;
-//        imagePicker.sourceType = .photoLibrary
+       // imagePicker.sourceType = .camera;
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = false;
     }
-
+    
+    //iuser has picked an image or movie
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage;
+        }
+        
+        imagePicker.dismiss(animated: true, completion:nil)
+        
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker,animated: true,completion: nil)
     }
